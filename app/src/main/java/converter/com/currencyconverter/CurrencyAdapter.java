f0c815ea6e925007801953f642af58278f75aac4
saikiranpadmanabuni <saikiran.padmanabuni46@gmail.com>
2015-10-28 23:54:55 +0530

    Finial touch up done.

--- app/src/main/java/converter/com/currencyconverter/CurrencyAdapter.java
+++ app/src/main/java/converter/com/currencyconverter/CurrencyAdapter.java
@@ -14,14 +14,16 @@ public class CurrencyAdapter extends BaseAdapter {
 	int selectedPosition = 0;
 
 	double conversion[][] = {
-		{1, 0.7289, 1.12974, 136.156, 1.57155, 1.49167},
-		{1.371930306, 1, 1.549924544, 186.796542736, 2.156057072, 2.046467279},
-		{0.885159417,0.645192699,1,120.51976561,1.391072282,1.320365748},
-		{0.007344517, 0.005353418,0.008297394,1, 0.011542275, 0.010955595 },
-		{0.636314467, 0.463809615, 0.718869906, 86.638032516,1, 0.9491712 },
-		{0.670389563, 0.488646953, 0.757365905, 91.277561391, 1.053550718, 1}};
+			{1, 0.7289, 1.12974, 136.156, 1.57155, 1.49167},
+			{1.371930306, 1, 1.549924544, 186.796542736, 2.156057072, 2.046467279},
+			{0.885159417, 0.645192699, 1, 120.51976561, 1.391072282, 1.320365748},
+			{0.007344517, 0.005353418, 0.008297394, 1, 0.011542275, 0.010955595},
+			{0.636314467, 0.463809615, 0.718869906, 86.638032516, 1, 0.9491712},
+			{0.670389563, 0.488646953, 0.757365905, 91.277561391, 1.053550718, 1}};
 
-	public CurrencyAdapter(Context context){
+	String country[] = {"Euros", "Pounds", "US Dollars", "Japanese Yen", "Austrailian Dollars", "Canadian Dollars"};
+
+	public CurrencyAdapter(Context context) {
 		mContext = context;
 	}
 
@@ -48,9 +50,9 @@ public class CurrencyAdapter extends BaseAdapter {
 
 		TextView text = (TextView) convertView.findViewById(R.id.textView);
 
-		double targetAmount = sourceAmount  * conversion[selectedPosition][position];
+		double targetAmount = sourceAmount * conversion[selectedPosition][position];
 
-		text.setText(targetAmount+"");
+		text.setText(targetAmount + " " + country[position]);
 
 		return convertView;
 	}
